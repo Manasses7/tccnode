f9954f6bafacf8b93d888e4b199284b4b66cc74e
Manassés Velho <134559013+Manasses7@users.noreply.github.com>
2023-11-24 19:25:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ app.use(express.static('./public'));
 
 app.use(require('body-parser').urlencoded({extended:true}));
 
-app.get ('/login', (req, res)=>{
-    res.redirect('./index.html')
+app.post('/login', (req, res)=>{
+   
+    const db = new sqlite3.Database('./banco_de_dados/login.db', sqlite3.OPEN_READ);
+    const sql = `SELECT email, senha FROM cadastro WHERE email=? AND senha=?`
+    db.get(sql, [req.body.email, req.body.password], (err, linha)=>{
+        if(linha) {
+            res.json({
+                status: 'Ok',
+                msg:'Login válido'
+            });
+        } else {
+            res.json({
+                status: 'Falhou',
+                msg: 'Login inválido'
+            })
+        }
+
+    })
 })
 
 app.post('/cadastro', (req, res)=>{ 
@@ -33,7 +49,7 @@ app.post('/cadastro', (req, res)=>{
             if(erro) {
                 res.json({msg: 'conta existente!'})
             } else {
-                res.redirect('/login')
+                res.redirect('/index.html')
             }
         }
     )
@@ -42,3 +58,4 @@ app.post('/cadastro', (req, res)=>{
 app.listen(3030, ()=>{
     console.log('servidor rodando na URL http://localhost:3030');
 })
+
